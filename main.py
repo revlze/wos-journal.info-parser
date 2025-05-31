bbbc32322e3d2783642c936bf6b08bdf2d4ff597
Platon U. <revlze@yahoo.com>
2025-05-31 21:43:37 +0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import logging
+from wos_journal_parser.downloader import Downloader
+from wos_journal_parser import logging_config 
+from wos_journal_parser.html_parser import JournalHtmlParser
+from wos_journal_parser.serializer import JournalSerializer
+
+logger = logging.getLogger(__name__)
+
+logger.info(f"Starting downloader")
+# with Downloader() as d:
+#   d.create_raw_dir()
+#   d.download()
+
+parser = JournalHtmlParser()
+journals = parser.parse_journals()
+
+save = JournalSerializer()
+save.save_journal_to_csv(journals=journals)
